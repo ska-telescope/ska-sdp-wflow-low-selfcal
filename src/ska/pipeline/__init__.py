bfe6ebdd418816a3cbc2651e5039d6abb4a9d8d7
chiarasal <chiara.salvoni@cgi.com>
2023-03-23 15:28:48 +0100

    AST-1204: remove example folders and add relevant ones

---
+++ src/ska/pipeline/__init__.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+
+"""Module init code."""
+
+
+__version__ = '0.0.0'
+
+__author__ = 'Chiara Salvoni'
+__email__ = 'chiara.salvoni@cgi.com'
+
+from .main import main
--- tests/test_example.py
+++ tests/test_example.py
@@ -4,37 +4,9 @@
 """Tests for the ska_python_skeleton module."""
 import pytest
 
-from ska.skeleton import SKA, function_example
+from ska.pipeline import main
 
-
-# TODO: Replace all the following examples with tests for the ska_python_skeleton package code
 def test_something():
     """Example: Assert with no defined return value."""
-    assert True
+    assert main() == "Test"
 
-
-def test_with_error():
-    """Example: Assert raising error."""
-    with pytest.raises(ValueError):
-        # Do something that raises a ValueError
-        raise ValueError
-
-
-# Fixture example
-@pytest.fixture
-def an_object():
-    """Example: Define fixture for subsequent test."""
-    return {}
-
-
-def test_ska_python_skeleton(an_object):
-    """Example: Assert fixture return value."""
-    assert an_object == {}
-
-
-def test_package():
-    """Example: Assert the ska_python_skeleton package code."""
-    assert function_example() is None
-    foo = SKA()
-    assert foo.example_2() == 2
-    assert foo.example() is None
