dba5e6f59cab0ce0fe622992bf28b37b264bc8d5
chiarasal <chiara.salvoni@cgi.com>
2023-03-23 17:01:06 +0100

    AST-1204: fix test

--- tests/test_example.py
+++ tests/test_example.py
@@ -4,7 +4,7 @@
 """Tests for the ska_sdp_wflow_low_selfcal module."""
 import pytest
 
-from ska.pipeline import main
+from ska_sdp_wflow_low_selfcal.pipeline import main
 
 def test_example():
     assert main() == "This is a dummy workflow script"
