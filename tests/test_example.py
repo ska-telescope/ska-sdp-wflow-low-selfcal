e8f5fb5610762d3e36e7be1b8019c6ecdf535f85
chiarasal <chiara.salvoni@cgi.com>
2023-03-31 18:23:29 +0200

    AST-1237: Setup project with poetry

--- tests/test_example.py
+++ tests/test_example.py
@@ -2,10 +2,9 @@
 # -*- coding: utf-8 -*-
 
 """Tests for the ska_sdp_wflow_low_selfcal module."""
-import pytest
+from ska_sdp_wflow_low_selfcal.pipeline.main import run_pipeline
 
-from ska_sdp_wflow_low_selfcal.pipeline import main
 
 def test_example():
-    assert main() == "This is a dummy workflow script"
-
+    "Dummy test"
+    assert run_pipeline() == "This is a dummy workflow script"
