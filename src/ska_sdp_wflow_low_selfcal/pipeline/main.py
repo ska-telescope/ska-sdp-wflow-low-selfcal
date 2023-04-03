c424aab3b3db135a2f8ccfb6e17f15c3ae792abf
Chiara Salvoni <chiara.salvoni@cgi.com>
2023-04-03 08:38:09 +0000

    AST-1237 change repository setup to follow template in https://gitlab.com/ska-telescope/ska-cicd-training-pipeline-machinery

--- src/ska_sdp_wflow_low_selfcal/pipeline/main.py
+++ src/ska_sdp_wflow_low_selfcal/pipeline/main.py
@@ -1,5 +1,13 @@
 # -*- coding: utf-8 -*-
+" This script sefines a SKA LOW self calibration workflow"
+
 
 def main():
+    "Main function defining the workflow"
 
+    run_pipeline()
+
+
+def run_pipeline():
+    "Run the workflow"
     return "This is a dummy workflow script"
