f7a664de414932b24d30bb6223c0d0c258d692ea
chiarasal <chiara.salvoni@cgi.com>
2023-04-03 13:09:45 +0200

    AST-1205: add tests for DP3

---
+++ tests/test_dp3.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+
+"""Tests for the ska_sdp_wflow_low_selfcal module."""
+from ska_sdp_wflow_low_selfcal.pipeline.dp3_helper import run_dp3
+
+
+def test_predict():
+    """Test DP3 predict"""
+    # run_dp3("/var/scratch/csalvoni/data/tNDPPP-generic.MS", "predict")
+    assert True
+
+
+def test_calibrate_phaseonly():
+    """Test DP3 phase only calibration"""
+    run_dp3(
+        "/var/scratch/csalvoni/data/tNDPPP-generic.MS", "calibrate_phaseonly"
+    )
+    assert True
+
+
+def test_calibrate_complex():
+    """Test DP3 calibration"""
+    assert True
