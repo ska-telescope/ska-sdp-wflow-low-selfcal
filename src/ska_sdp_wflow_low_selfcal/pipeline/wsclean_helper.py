b694e8347c2cdb869413a37cd56cba4ad0410267
chiarasal <chiara.salvoni@cgi.com>
2023-04-03 13:09:32 +0200

    AST-1205 do formatting

--- src/ska_sdp_wflow_low_selfcal/pipeline/wsclean_helper.py
+++ src/ska_sdp_wflow_low_selfcal/pipeline/wsclean_helper.py
@@ -1,14 +1,22 @@
+""" This module contains commands to use WSClean """
 from subprocess import check_call
 
-
 common_args = [
-            "-size", "512", "512",
-            "-scale", "0.01",
-            "-channel-range", "0", "1",
-            "-name", "/var/scratch/csalvoni/data/dummy",
-        ]
+    "-size",
+    "512",
+    "512",
+    "-scale",
+    "0.01",
+    "-channel-range",
+    "0",
+    "1",
+    "-name",
+    "/var/scratch/csalvoni/data/dummy",
+]
+
 
 def run_wsclean(msin):
+    """Run WSClean"""
     check_call(
         [
             "/home/csalvoni/scratch/schaap/wsclean/build/wsclean",
