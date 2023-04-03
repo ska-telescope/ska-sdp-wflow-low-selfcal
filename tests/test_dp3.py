7b95ca532ccbf361ecc0c37c383612e055edaca6
chiarasal <chiara.salvoni@cgi.com>
2023-04-03 16:01:24 +0200

    AST-1205: add test data

--- tests/test_dp3.py
+++ tests/test_dp3.py
@@ -3,21 +3,28 @@
 """Tests for the ska_sdp_wflow_low_selfcal module."""
 from ska_sdp_wflow_low_selfcal.pipeline.dp3_helper import run_dp3
 
+TEST_DATA="tests/test_data/"
 
-def test_predict():
-    """Test DP3 predict"""
-    run_dp3("/var/scratch/csalvoni/data/tNDPPP-generic.MS", "predict")
-    assert True
-
-
-def test_calibrate_phaseonly():
+def test_pipeline_phaseonly():
     """Test DP3 phase only calibration"""
-    run_dp3(
-        "/var/scratch/csalvoni/data/tNDPPP-generic.MS", "calibrate_phaseonly"
-    )
+    run_dp3(f"{TEST_DATA}/tNDPPP-generic.MS", "calibrate_phaseonly")
+
+    # assert that the h5parm is created and contains the right fields
+
+    run_dp3(f"{TEST_DATA}/tNDPPP-generic.MS", "predict")
+
     assert True
 
 
-def test_calibrate_complex():
+def test_pipeline_complex():
     """Test DP3 calibration"""
+
+    run_dp3(f"{TEST_DATA}/tNDPPP-generic.MS", "calibrate_scalarphase")
+    run_dp3(f"{TEST_DATA}/tNDPPP-generic.MS", "calibrate_complexgain")
+
+    # assert that the h5parm is created and contains the right fields
+
+
+    run_dp3(f"{TEST_DATA}/tNDPPP-generic.MS", "predict")
+
     assert True
