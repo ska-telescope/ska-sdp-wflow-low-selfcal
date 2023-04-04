9a0d41f808c0fc5745217759dde7183f096550ee
chiarasal <chiara.salvoni@cgi.com>
2023-04-04 18:54:01 +0200

    AST-1205: use fixture in common file

---
+++ tests/conftest.py
@@ -0,0 +1,59 @@
+""" In this module all the fixture to run the python tests are defined """
+import os
+import shutil
+import uuid
+from subprocess import check_call
+
+import pytest
+
+TEST_DATA = "../tests/test_data"
+
+
+@pytest.fixture(scope="session", autouse=False)
+def source_env(run_dp3=True, run_wsclean=True):
+    """Define a temporary folder to run the test. The folder is deleted
+    once the test has been run"""
+
+    ms_name = "tNDPPP-generic.MS"
+    current_directory = os.getcwd()
+
+    os.chdir(current_directory)
+    tmpdir = str(uuid.uuid4())
+    os.mkdir(tmpdir)
+    os.chdir(tmpdir)
+    source = f"{TEST_DATA}/{ms_name}.tgz"
+    if not os.path.isfile(source):
+        raise IOError(f"Not able to find {source}.")
+    check_call(["tar", "xf", source])
+
+    if run_dp3:
+        copy_data_dp3()
+    if run_wsclean:
+        copy_data_wsclean()
+    # Tests are executed here
+    yield
+
+    # Post-test: clean up
+    os.chdir(current_directory)
+    shutil.rmtree(tmpdir)
+
+
+def copy_data_dp3():
+    """Copy data needed for DP3 tests into temporary folder"""
+    skymodel_path = f"{TEST_DATA}/grouped.skymodel"
+    if not os.path.isfile(skymodel_path):
+        raise IOError(f"Not able to find {skymodel_path}.")
+    shutil.copy(skymodel_path, "grouped.skymodel")
+
+
+def copy_data_wsclean():
+    """Copy data needed for WSClean tests into temporary folder"""
+    facets_path = f"{TEST_DATA}/facets.reg"
+    if not os.path.isfile(facets_path):
+        raise IOError(f"Not able to find {facets_path}.")
+    shutil.copy(facets_path, "facets.reg")
+
+    h5parm_path = f"{TEST_DATA}/fast_phase_0.h5parm"
+    if not os.path.isfile(h5parm_path):
+        raise IOError(f"Not able to find {h5parm_path}.")
+    shutil.copy(h5parm_path, "fast_phase_0.h5parm")
--- tests/test_wsclean.py
+++ tests/test_wsclean.py
@@ -1,45 +1,7 @@
 """Tests for the ska_sdp_wflow_low_selfcal module."""
-import os
-import shutil
-import uuid
-from subprocess import check_call
-
-import pytest
-
 from ska_sdp_wflow_low_selfcal.pipeline.wsclean_helper import run_wsclean
 
-TEST_DATA = "../tests/test_data"
 MSIN = "tNDPPP-generic.MS"
-CWD = os.getcwd()
-
-
-@pytest.fixture(autouse=True)
-def source_env():
-    os.chdir(CWD)
-    tmpdir = str(uuid.uuid4())
-    os.mkdir(tmpdir)
-    os.chdir(tmpdir)
-    source = f"{TEST_DATA}/{MSIN}.tgz"
-    if not os.path.isfile(source):
-        raise IOError(f"Not able to find {source}.")
-    check_call(["tar", "xf", source])
-
-    facets_path = f"{TEST_DATA}/facets.reg"
-    if not os.path.isfile(facets_path):
-        raise IOError(f"Not able to find {facets_path}.")
-    shutil.copy(facets_path, "facets.reg")
-
-    h5parm_path = f"{TEST_DATA}/fast_phase_0.h5parm"
-    if not os.path.isfile(h5parm_path):
-        raise IOError(f"Not able to find {h5parm_path}.")
-    shutil.copy(h5parm_path, "fast_phase_0.h5parm")
-
-    # Tests are executed here
-    yield
-
-    # Post-test: clean up
-    os.chdir(CWD)
-    shutil.rmtree(tmpdir)
 
 
 def test_wsclean():
