650e3cfa9143563e7b37b28c816b1a34323b2ec6
chiarasal <chiara.salvoni@cgi.com>
2023-04-05 16:13:17 +0200

    AST-1205: skip tests when module not available

--- tests/test_dp3.py
+++ tests/test_dp3.py
@@ -3,14 +3,26 @@
 """Tests for the ska_sdp_wflow_low_selfcal module."""
 
 
+import shutil
+
+import pytest
+
 from ska_sdp_wflow_low_selfcal.pipeline.dp3_helper import Dp3Runner
 
 MSIN = "tNDPPP-generic.MS"
+PATH_TO_DP3_EXE = "/home/csalvoni/scratch/schaap/dp3/build/DP3"
+
+
+@pytest.fixture(autouse=True)
+def is_dp3_installed():
+    """Check if DP3 is installed, and skip test if not"""
+    if shutil.which(PATH_TO_DP3_EXE) is None:
+        pytest.skip("Skipping test as DP3 is not installed")
 
 
 def test_pipeline_phaseonly(create_environment):  # pylint: disable=W0613
     """Test DP3 phase only calibration"""
-    dp3_runner = Dp3Runner("/home/csalvoni/scratch/schaap/dp3/build/DP3")
+    dp3_runner = Dp3Runner(PATH_TO_DP3_EXE)
     # Optional: read directions from skymodel
     directions = (
         "[[Patch_0],[Patch_1],[Patch_10],[Patch_11],[Patch_12],"
@@ -44,7 +56,7 @@ def test_pipeline_phaseonly(create_environment):  # pylint: disable=W0613
 def test_pipeline_complex(create_environment):  # pylint: disable=W0613
     """Test DP3 calibration"""
 
-    dp3_runner = Dp3Runner("/home/csalvoni/scratch/schaap/dp3/build/DP3")
+    dp3_runner = Dp3Runner(PATH_TO_DP3_EXE)
     # Optional: read directions from skymodel
     dp3_runner.calibrate_scalarphase(
         f"{MSIN}",
