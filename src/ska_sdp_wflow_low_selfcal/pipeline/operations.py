1455f052c8145732a5236bc4f4bd098182469355
chiarasal <chiara.salvoni@cgi.com>
2023-04-07 16:39:43 +0200

    AST-1244: add calibrate_2,calibrate_3, predict_3, image_3

--- src/ska_sdp_wflow_low_selfcal/pipeline/operations.py
+++ src/ska_sdp_wflow_low_selfcal/pipeline/operations.py
@@ -40,6 +40,76 @@ def calibrate_1(dp3_runner):
     # Missing 2: get outlier_1_predict_skymodel (implement in AST-1236)
 
 
+def calibrate_2(dp3_runner):
+    """Define calibrate operation"""
+    # ------------------------ Calibrate_2 1:04:16
+    logging.info("Start calibrate_2")
+    msin_after_predict = [
+        f"{WORK_DIR}/predict_1/midbands.ms.mjd5020557063_field",
+        f"{WORK_DIR}/predict_1/midbands.ms.mjd5020559947_field",
+        f"{WORK_DIR}/predict_1/midbands.ms.mjd5020562823_field",
+        f"{WORK_DIR}/predict_1/midbands.ms.mjd5020565707_field",
+        f"{WORK_DIR}/predict_1/midbands.ms.mjd5020568583_field",
+        f"{WORK_DIR}/predict_1/midbands.ms.mjd5020571459_field",
+        f"{WORK_DIR}/predict_1/midbands.ms.mjd5020574343_field",
+        f"{WORK_DIR}/predict_1/midbands.ms.mjd5020577219_field",
+        f"{WORK_DIR}/predict_1/midbands.ms.mjd5020580103_field",
+        f"{WORK_DIR}/predict_1/midbands.ms.mjd5020582979_field",
+    ]
+
+    for i, start_time in enumerate(START_TIMES):
+        dp3_runner.calibrate_scalarphase(
+            msin_after_predict[i],
+            start_time,
+            f"{WORK_DIR}/inputs/in_calibration_2.txt",
+            False,
+            f"{WORK_DIR}/outputs/out_calibration_2_fast_phase_"
+            + str(i)
+            + ".h5parm",
+        )
+
+    # Missing 1: stitch all solutions together
+    # Shortcut: get solutions from rapthor output
+    # Implement in AST-1236
+
+    # Missing 2: get outlier_1_predict_skymodel (implement in AST-1236)
+
+
+def calibrate_3(dp3_runner):
+    """Define calibrate operation"""
+    # ------------------------ Calibrate_1 0:52:21
+    # logging.info("Start calibrate_1")
+    # for i, start_time in enumerate(START_TIMES):
+    #     dp3_runner.calibrate_scalarphase(
+    #         f"{MSIN}",
+    #         start_time,
+    #         f"{WORK_DIR}/inputs/in_calibration_3.txt",
+    #         True,
+    #         f"{WORK_DIR}/outputs/out_calibration_3_fast_phase_"
+    #         + str(i)
+    #         + ".h5parm",
+    #     )
+
+    # missing: collect solutions together -> H5parm_collector
+    for i, start_time in enumerate(START_TIMES):
+        dp3_runner.calibrate_complexgain(
+            f"{MSIN}",
+            start_time,
+            f"{WORK_DIR}/inputs/in_calibration_3.txt",
+            f"{WORK_DIR}/inputs/out_calibration_3_fast_phase.h5parm",
+            f"{WORK_DIR}/outputs/out_calibration_3_slow_gain_separate_"
+            + str(i)
+            + ".h5parm",
+        )
+
+    # Missing 1: stitch all solutions together
+    # Shortcut: get solutions from rapthor output
+    # Implement in AST-1236
+    # combine combine_h5parms.py -> slow_gains_separate.h5parm
+
+    # Missing 2: get outlier_1_predict_skymodel (implement in AST-1236)
+
+
 def predict_1(dp3_runner):
     """Define predict operation"""
     # ------------------------ Predict_1 0:18:10
@@ -64,7 +134,44 @@ def predict_1(dp3_runner):
     # IN: outlier_1_modeldata -> OUT: mjd5020559947_field
 
 
-def image_1(dp3_runner, wsclean_runner):
+def predict_3(dp3_runner):
+    """Define predict operation"""
+
+    directions_1 = "[[Patch_1022],[Patch_1042],[Patch_1075],[Patch_136],\
+    [Patch_151],[Patch_152],[Patch_231],[Patch_235],[Patch_313],[Patch_34],\
+    [Patch_341]]"
+    logging.info("Start Predict_1")
+    for i, start_time in enumerate(START_TIMES):
+        msout = f"midbands.ms.{i}.outlier_1_modeldata"
+        dp3_runner.predict(
+            MSIN,
+            msout,
+            start_time,
+            directions_1,
+            f"{WORK_DIR}/inputs/bright_source_2_predict_skymodel.txt",
+            f"{WORK_DIR}/outputs/out_calibration_2_fast_phase_"
+            f"{WORK_DIR}/inputs/in_predict_3_field-solutions.h5",
+        )
+
+    directions_2 = "[[Patch_341],[Patch_375],[Patch_423],[Patch_456],\
+    [Patch_47],[Patch_479],[Patch_51],[Patch_809],[Patch_865],[Patch_900]]"
+    logging.info("Start Predict_1")
+    for i, start_time in enumerate(START_TIMES):
+        msout = f"midbands.ms.{i}.outlier_1_modeldata"
+        dp3_runner.predict(
+            MSIN,
+            msout,
+            start_time,
+            directions_2,
+            f"{WORK_DIR}/inputs/bright_source_2_predict_skymodel.txt",
+            f"{WORK_DIR}/outputs/out_calibration_2_fast_phase_"
+            f"{WORK_DIR}/inputs/in_predict_3_field-solutions.h5",
+        )
+    # Missing 3: subtract_sector_models.py * 10 times
+    # IN: outlier_1_modeldata -> OUT: mjd5020559947_field
+
+
+def image_1(dp3_runner, wsclean_runner, solutions_to_apply):
     """Define imaging operation"""
     # ------------------------ Image_1 7:06:40
     logging.info("Start Image_1")
@@ -110,6 +217,12 @@ def image_1(dp3_runner, wsclean_runner):
     ]
 
     facets_file = f"{WORK_DIR}/inputs/sector_1_facets_ds9.reg"
-    solutions_file = (f"{WORK_DIR}/inputs/field-solutions.h5",)
+    solutions_to_apply = f"{WORK_DIR}/inputs/field-solutions_calibration_1.h5"
 
-    wsclean_runner.run_wsclean(input_imaging, facets_file, solutions_file)
+    wsclean_runner.run_wsclean(input_imaging, facets_file, solutions_to_apply)
+
+
+def image_3(dp3_runner, wsclean_runner, solutions_to_apply):
+    """Define imaging operation"""
+
+    # do nothing
