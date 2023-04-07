96c0e584abec039e719ca796c6cd55b77ba800f8
chiarasal <chiara.salvoni@cgi.com>
2023-04-07 16:40:05 +0200

    AST-1244: add tests for new functions

--- tests/test_steps.py
+++ tests/test_steps.py
@@ -6,6 +6,8 @@
 from ska_sdp_wflow_low_selfcal.pipeline.dp3_helper import Dp3Runner
 from ska_sdp_wflow_low_selfcal.pipeline.operations import (
     calibrate_1,
+    calibrate_2,
+    calibrate_3,
     image_1,
     predict_1,
 )
@@ -21,6 +23,12 @@ def test_calibrate_1():
     calibrate_1(dp3_runner)
 
 
+def test_calibrate_2():
+    """Test calibrate_2"""
+    dp3_runner = Dp3Runner(PATH_TO_DP3_EXE)
+    calibrate_2(dp3_runner)
+
+
 def test_predict_1():
     """Test predict_1"""
     dp3_runner = Dp3Runner(PATH_TO_DP3_EXE)
@@ -31,4 +39,16 @@ def test_image_1():
     """Test image_1"""
     dp3_runner = Dp3Runner(PATH_TO_DP3_EXE)
     wsclean_runner = WSCleanRunner(PATH_TO_WSCLEAN_EXE)
-    image_1(dp3_runner, wsclean_runner)
+    image_1(
+        dp3_runner,
+        wsclean_runner,
+        "tests/test_data/field-solutions_calibration_2.h5",
+    )
+
+
+def test_calibrate_3():
+    """Test calibrate_3"""
+    dp3_runner = Dp3Runner(PATH_TO_DP3_EXE)
+    calibrate_3(
+        dp3_runner,
+    )
