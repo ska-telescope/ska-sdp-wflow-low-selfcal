a6b4ba8a4cd89e284ced048c88c41d9590e2801c
chiarasal <chiara.salvoni@cgi.com>
2023-04-14 10:48:34 +0200

    AST-1236: fix TEST_DATA path

--- tests/conftest.py
+++ tests/conftest.py
@@ -5,9 +5,7 @@ from subprocess import check_call
 
 import pytest
 
-TEST_DATA = "/home/csalvoni/schaap/ska-sdp-wflow-low-selfcal-branch/\
-ska-sdp-wflow-low-selfcal/tests/test_data"
-
+TEST_DATA = "tests/test_data"
 CWD = os.getcwd()
 
 
@@ -15,14 +13,13 @@ CWD = os.getcwd()
 def source_env(tmp_path, run_dp3=True, run_wsclean=True):
     """Define a temporary folder to run the test. The folder is deleted
     once the test has been run"""
+
     os.chdir(tmp_path)
 
     ms_name = "tNDPPP-generic.MS"
-    source = f"{TEST_DATA}/{ms_name}.tgz"
+    source = f"{CWD}/{TEST_DATA}/{ms_name}.tgz"
     if not os.path.isfile(source):
         raise IOError(f"Not able to find {source}.")
-    check_call(["tar", "xf", source])
-    # untar_ms(f"{tcf.RESOURCEDIR}/{MSIN}.tgz")
     check_call(["tar", "xf", source])
 
     if run_dp3:
@@ -60,7 +57,7 @@ def copy_data_support_functions():
         "sector_1-sources-pb.txt",
     ]
     for filename in files_to_copy:
-        file_path = f"{TEST_DATA}/{filename}"
+        file_path = f"{CWD}/{TEST_DATA}/{filename}"
         if not os.path.isfile(file_path):
             raise IOError(f"Not able to find {file_path}.")
         shutil.copy(file_path, filename)
@@ -68,7 +65,7 @@ def copy_data_support_functions():
 
 def copy_data_dp3():
     """Copy data needed for DP3 tests into temporary folder"""
-    skymodel_path = f"{TEST_DATA}/grouped.skymodel"
+    skymodel_path = f"{CWD}/{TEST_DATA}/grouped.skymodel"
     if not os.path.isfile(skymodel_path):
         raise IOError(f"Not able to find {skymodel_path}.")
     shutil.copy(skymodel_path, "grouped.skymodel")
@@ -76,7 +73,7 @@ def copy_data_dp3():
 
 def copy_data_wsclean():
     """Copy data needed for WSClean tests into temporary folder"""
-    facets_path = f"{TEST_DATA}/facets.reg"
+    facets_path = f"{CWD}/{TEST_DATA}/facets.reg"
     if not os.path.isfile(facets_path):
         raise IOError(f"Not able to find {facets_path}.")
     shutil.copy(facets_path, "facets.reg")
